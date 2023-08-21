52282c53035e8a819403114a15dd8963fc099c29
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-08-21 11:09:20 +0100

    Better error handling

--- src/main.c
+++ src/main.c
@@ -16,14 +16,44 @@ bool background_f = false;
 bool help_f = false;
 bool playback_f = false;
 
+/* used to represent no flags 
+ * if args are supplied but no flags
+ * the program will fail */
+bool NULL_f = true;
+
 /* file that will be played */
 char *file_name;
 
 /* supported file types */
 const char *valid_ex[] = {
-	"wav",
-	"mp3",
+	".wav",
+	".mp3",
 };
+
+/* gets file type from string */
+static char *get_file_type (char *str)
+{
+	char *ex = malloc(sizeof(str));
+	size_t len = strlen(str), dot_index = 0;
+
+	if (ex == NULL) 
+	{
+		printe("malloc (ex) get_file_type");
+		return NULL;
+	}
+
+	for (int i = 0; str[i] != (char) 0; i++)
+		if (str[i] == '.')
+		{
+			dot_index = i;
+			break;
+		}
+
+	for (size_t j = dot_index; str[j] != (char) 0; j++)
+		strncat(ex, &str[j], 1);
+
+	return ex;
+}
 
 static int help (void)
 {
@@ -31,10 +61,7 @@ static int help (void)
 	fprintf(stdout, "-p [file] to play a file\n");
 	fprintf(stdout, "-b to run in background\n");
 	fprintf(stdout, "Supported file types:\n");
-
-	for (int i = 0; valid_ex[i] != NULL; i++)
-		fprintf(stdout, "%s\n", valid_ex[i]);
-
+	fprintf(stdout, "\twav\n\tmp3\n");
 	return 0;
 }
 
@@ -54,39 +81,48 @@ static bool is_file (const char *fn)
 static int check_extension (char *ex)
 {
 	for (size_t i = 0; valid_ex[i] != NULL; i++)
-		if (strncmp(valid_ex[i], ex, strlen(valid_ex[i])))
+		if (strncmp(valid_ex[i], ex, strlen(valid_ex[i])) == 0)
 			return 0;
 
 	return 1;
 }
 
 /* sets the file name */ 
-static void set_file (char *name)
+static int set_file (char *name)
 {
 	char *ex = malloc(sizeof(name));
-
-	if (ex == NULL)
-	{
-		printe("malloc (ex) set_file");
-		return;
-	}
-
-	if (is_file(name) == false)
-	{
-		printe("no such file or directory");
-		return;
-	}
-
-	if (check_extension(ex) != 0)
-	{
-		printe("invalid file type");
-		return;
-	}
 
 	if (name == NULL)
 	{
 		printe("No file supplied!\n");
-		return;
+		return 1;
+	}
+
+	if (ex == NULL)
+	{
+		printe("malloc (ex) set_file");
+		return 1;
+	}
+
+	/* making sure file exists */
+	if (is_file(name) == false)
+	{
+		printe("no such file or directory");
+		return 1;
+	}
+
+	/* getting the file extension */
+	if ((ex = get_file_type(name)) == NULL)
+	{
+		printe("invalid file type");
+		return 1;
+	}
+
+	/* checks extension is valid */
+	if (check_extension(ex) != 0)
+	{
+		printe("invalid file type");
+		return 1;
 	}
 
 	if (sizeof(name) > sizeof(file_name))
@@ -94,35 +130,49 @@ static void set_file (char *name)
 
 	file_name = strndup(name, strlen(name));
 	free(ex);
+	return 0;
 }
 
 /* scans command line args and sets bools */
 static int scan_args (char *argv[])
 {
+	int file_index = 0;
+
+	/* scanning args */
 	for (size_t i = 0; argv[i] != NULL; i++)
 		if (argv[i][0] == '-' && argv[i][2] == (char) 0)
 			switch (argv[i][1])
 			{
 				case 'p':
 					playback_f = true;
-					set_file(argv[i+1]);
+					file_index = i+1;
+					NULL_f = false;
 					break;
 				case 'h':
 					help_f = true;
+					NULL_f = false;
 					break;
 				case 'b':
 					background_f = true;
+					NULL_f = false;
 					break;
 				default:
 					fprintf(stderr, "ERROR: No such arg %s\n", argv[i]);
 					return 1;
-					break;
 			}
 		else if (argv[i][0] == '-' && argv[i][2] != (char) 0)
 		{
 			fprintf(stderr, "ERROR: No such arg %s\n", argv[i]);
 			return 1;
 		}
+
+	/* setting the file name */
+	if (set_file(argv[file_index]) != 0) 
+	{
+		playback_f = false;
+		NULL_f = true;
+		return 1;
+	}
 
 	return 0;
 }
@@ -136,10 +186,16 @@ static int setup (int argc, char *argv[])
 		printe("malloc (file_name) setup");
 		return 1;
 	}
-
+	
 	if (argc < 2) return curses_setup();
 
 	if (scan_args(argv) != 0) return 1;
+
+	if (NULL_f == true)
+	{
+		printe("no flags supplied see -h");
+		return 1;
+	}
 
 	if (help_f == true) return help();
 
